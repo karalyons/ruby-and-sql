2161ad5f7a543985887a97b78b5a51e02b630865
karalyons <kara.lyons@kellogg.northwestern.edu>
2023-01-31 22:09:29 +0000

    updated associations

--- code-along/3-associations.rb
+++ code-along/3-associations.rb
@@ -12,6 +12,37 @@ Contact.destroy_all
 
 # 1. insert new rows in the contacts table with relationship to a company
 
+
+apple = Company.find_by({"name" => "Apple"})
+#puts apple.inspect
+
+contact = Contact.new
+contact["first_name"] = "Tim"
+contact["last_name"] = "Cook"
+contact["email"] = "timapple.com"
+contact["company_id"] = apple["id"]
+contact.save
+
+contact = Contact.new
+contact["first_name"] = "Manny"
+contact["last_name"] = "Mr. Man"
+contact["email"] = "manman@apple.com"
+contact["company_id"] = apple["id"]
+contact.save
+
+
+amazon = Company.find_by({"name" => "Amazon"})
+contact = Contact.new
+contact["first_name"] = "Jeff"
+contact["last_name"] = "Bezos"
+contact["email"] = "bezos@amazon.com"
+contact["company_id"] = amazon["id"]
+contact.save
+
+puts "There are #{Company.all.count} companies"
+puts "There are #{Contact.all.count} contacts"
 # 2. How many contacts work at Apple?
 
+apple_contacts = Contact.where({"company_id" => apple["id"]})
+puts "Apple Contacts: #{apple_contacts.count}"
 # 3. What is the full name of each contact who works at Apple?
