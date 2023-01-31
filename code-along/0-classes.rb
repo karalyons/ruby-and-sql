618ecdd8a966d093ecf2dae70fe7376a6c87b4d5
karalyons <kara.lyons@kellogg.northwestern.edu>
2023-01-31 20:29:54 +0000

    made tables

--- code-along/0-classes.rb
+++ code-along/0-classes.rb
@@ -5,4 +5,27 @@
 
 # String Class
 
+bens_favorite_food = "sushi"
+brians_favorite_food = "tacos"
+
+puts bens_favorite_food.class # type of thing this is (aka object)
+puts brians_favorite_food.class # type of thing this is (aka object)
+
+# String Methods
+
+puts bens_favorite_food.upcase
+puts brians_favorite_food.upcase
+
+# Our own class
+class Dog
+
+  def speak
+    puts "Woof!"
+  end
+
+end
+
+jenkins = Dog.new
+jenkins.speak
+
 # String Methods
